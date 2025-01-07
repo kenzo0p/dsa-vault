2eac458e6ae539e5058330221b921a072baa3f45
Om Bhor <ombhor617@gmail.com>
2025-01-07 20:59:33 +0530

    opearators  in java

---
+++ operators/Basics.java
@@ -0,0 +1,38 @@
+package operators;
+
+public class Basics {
+    public static void main(String[] args) {
+        // arithmetic opearators
+
+        // +,-,*,/,%
+        int a = 10;
+        int b = 5;
+        // System.out.println("ad = " + (a+b));
+        // System.out.println("mul = " + (a*b));
+        // System.out.println("sub = " + (a - b));
+        // System.out.println("div = " + (a / b));
+
+        int c = --a;  // ++a
+        System.out.println(c);
+
+        //! relational operators
+        // == , != , > ,< ,>=,<=
+
+
+        //! logical opearators
+        // && , || , !
+
+        //!assignment operators
+        // == , +=, -= , *= , /=
+
+        // questions ----\
+        // 1 = , 5 , 5
+        // 2 = , java
+        // 3 = , 4,0,0
+        // 4 = 278
+        // 5 = 20,20
+
+
+        
+    }
+}
