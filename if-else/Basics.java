ee1ec61c54d6fee70ff13805e3ec01b5737c6d6c
Om Bhor <ombhor617@gmail.com>
2025-01-11 22:33:22 +0530

    Remove commented-out leap year calculation code from Basics.java

--- if-else/Basics.java
+++ if-else/Basics.java
@@ -159,13 +159,15 @@ class Basics {
         // sc.close();
 
         // another method
-        boolean x = (year %4) == 0;
-        boolean y = (year % 100) != 0;
-        boolean z = ((year % 100) == 0 && (year % 400 == 0));
-        if(x && (y || z)) {
-            System.out.println(year + " is a leap year");
-        }else {
-            System.out.println(year + " is not a leap year");
-        }
+        // boolean x = (year %4) == 0;
+        // boolean y = (year % 100) != 0;
+        // boolean z = ((year % 100) == 0 && (year % 400 == 0));
+        // if(x && (y || z)) {
+        //     System.out.println(year + " is a leap year");
+        // }else {
+        //     System.out.println(year + " is not a leap year");
+        // }
+
+        
     }
 }
