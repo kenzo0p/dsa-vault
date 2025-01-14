28c676b016bc37a4d15f2c1a6854f7c60318b1e8
Om Bhor <ombhor617@gmail.com>
2025-01-14 21:30:54 +0530

    Patterns Part 1

---
+++ Patterns-part-one/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of lines you want print : ");
+        int n = sc.nextInt();
+
+        // Star pattern
+        // for(int i = 1 ; i<=n;i++){
+        // for(int j =1;j<=i;j++){
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        // inverted star pattern
+        // for(int i =1 ; i<=n;i++){
+        // for(int j = i ; j<=n;j++){//for (int j =1;j<=n-i+1;
+        // j++){System.out.println("* ");}
+        // System.out.print("* ");
+
+        // }
+        // System.out.println();
+        // }
+
+        // half pyramid pattern
+
+        // for(int i =1;i<=n;i++){
+        // for(int number = 1;number <=i;number++){
+        // System.out.print(number+" ");
+        // }
+        // System.out.println();
+        // }
+
+        // character pattern
+        // char ch = 'A';
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(ch + " ");
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+
+        //! note
+        
+    }
+}
