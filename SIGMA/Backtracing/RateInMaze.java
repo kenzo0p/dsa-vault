1cec7d759c52e7108c6a5e5ab67db0a25a643fd2
Om Bhor <ombhor617@gmail.com>
2025-02-17 23:41:02 +0530

    add RateInMaze class for backtracking pathfinding in a maze

---
+++ SIGMA/Backtracing/RateInMaze.java
@@ -0,0 +1,34 @@
+public class RateInMaze {
+    static int [] dr = {-1,+1 , 0, 0};
+    static int [] dc = {0 , 0, +1 ,-1};
+    static char [] dir = {'U' ,'D','R','L' };
+    public static void rec(int[][] maze, int row , int col , String path){
+        if(row >= maze.length || col >= maze.length || col <0 || row < 0){
+            // matrix out of bound
+            return;
+
+        }
+        if(maze[row][col] == -1 || maze[row][col] == 1){
+
+            return;
+        }
+
+        if(row == maze.length-1 && col == maze.length-1){
+            System.out.println(path);
+            
+            return;
+        }
+        maze[row][col] = -1;
+        for(int idx = 0;idx<maze.length;idx++){
+            int nr = row+dr[idx];//new row
+            int nc = col + dc[idx];//new col
+            
+            rec(maze, nr, nc , path + dir[idx]);
+        }
+        maze[row][col] = 0;//unvisit mark
+    }
+    public static void main(String[]args){
+        int mat[][] = {{0,0,0,1} , {0,1,0,1} , {0,0,0,0} , {0,1,1,0}};
+        rec(mat, 0, 0 , "");
+    }
+}
