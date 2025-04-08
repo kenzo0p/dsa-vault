b48517ec9360260c99b16af850690a8a872fb1f2
Om Bhor <ombhor617@gmail.com>
2025-04-09 02:43:39 +0530

    Graph and dp concepts and question pt-3

--- SIGMA/DP/knapsack/ZeroOneKnapsack.java
+++ SIGMA/DP/knapsack/ZeroOneKnapsack.java
@@ -48,7 +48,7 @@ public class ZeroOneKnapsack {
     public static void print(int[][] dp) {
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
-                System.out.print(dp[i][j] + " ");
+                System.out.print(dp[i][j] + "   ");
             }
             System.out.println();
         }
---
+++ gfg-160/StockBuyandSellMaxoneTransactionAllowed.java
@@ -0,0 +1,5 @@
+public class StockBuyandSellMaxoneTransactionAllowed {
+    public static void main(String[] args) {
+        
+    }
+}
